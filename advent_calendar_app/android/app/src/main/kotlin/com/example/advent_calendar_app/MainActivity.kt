85024944da20e0505f842eae1d486527dac2a9aa
virtalas <lasvirt@gmail.com>
2022-10-12 20:43:45 +0300

    List of doors with open states

---
+++ advent_calendar_app/android/app/src/main/kotlin/com/example/advent_calendar_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.advent_calendar_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ advent_calendar_app/lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AdventCalendarApp());
+}
+
+class AdventCalendarApp extends StatefulWidget {
+  const AdventCalendarApp({super.key});
+
+  @override
+  State<AdventCalendarApp> createState() => _AdventCalendarAppState();
+}
+
+class _AdventCalendarAppState extends State<AdventCalendarApp> {
+  List<bool> _openStates = <bool>[true, true, true, true];
+
+  @override
+  Widget build(BuildContext context) {
+    final List<int> days = <int>[12, 11, 10, 9];
+
+    // MediaQuery.of(context).size.width
+
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(vertical: 80, horizontal: 0),
+          itemCount: days.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: (){
+                _toggleIsOpen(index);
+              },
+              child: CalendarDoor(day: days[index], isOpen: _openStates[index]),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) => const SizedBox(height: 64),
+        ),
+      ),
+    );
+  }
+
+  void _toggleIsOpen(int index) {
+    setState(() {
+      _openStates[index] = !_openStates[index];
+    });
+  }
+}
+
+class CalendarDoor extends StatelessWidget {
+  final int day;
+  final bool isOpen;
+
+  const CalendarDoor({super.key, required this.day, required this.isOpen});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CalendarSingleDoor(text: '${day}. lu', isOnLeft: true, isOpen: isOpen),
+        CalendarSingleDoor(text: 'ukku', isOnLeft: false, isOpen: isOpen),
+      ],
+    );
+  }
+}
+
+class CalendarSingleDoor extends StatelessWidget {
+  final String text;
+  final bool isOnLeft;
+  final bool isOpen;
+
+  const CalendarSingleDoor({super.key, required this.text, required this.isOnLeft, required this.isOpen});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: isOpen ? 100 : 150,
+      height: 400,
+      decoration: BoxDecoration(
+        color: const Color(0xff7c94b6),
+        border: Border.all(
+          width: 1,
+        ),
+      ),
+      child: Align(
+        alignment: isOnLeft ? Alignment.centerRight : Alignment.centerLeft,
+        child: Text(text, style: const TextStyle(fontSize: 30),),
+      ),
+    );
+  }
+}
