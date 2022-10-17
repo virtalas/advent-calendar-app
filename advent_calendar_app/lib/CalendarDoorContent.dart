7c566038e8d9a6d9f2318a8f2c2b3ee69479932e
virtalas <lasvirt@gmail.com>
2022-10-17 21:25:32 +0300

    Add CalendarDoorContent file. Add scale animation to image

---
+++ advent_calendar_app/lib/CalendarDoorContent.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'constants.dart' as constants;
+import 'package:snowfall/snowfall/snowflakes.dart';
+
+class CalendarDoorContent extends StatelessWidget {
+  Widget child;
+  bool isOpen;
+  CalendarDoorContent({super.key, required this.isOpen, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    final Widget image = SizedBox(
+      height: constants.doorHeight,
+      width: constants.doorHeight,
+      child: ClipRect(
+        child: AnimatedScale(
+          scale: isOpen ? 1 : 1.2,
+          curve: isOpen ? Curves.easeInSine : Curves.easeInOut,
+          duration: const Duration(milliseconds: constants.doorAnimationDuration),
+          child: Image.asset(
+            'assets/default.jpeg',
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+    );
+
+    final Widget snowfallAndDoors = Center(
+      // For some reason this SizedBox is needed to display the Stack.
+      child: SizedBox(
+        width: constants.doorHeight,
+        height: constants.doorHeight,
+        child: Stack(
+          children: [
+            Positioned.fill(child: ClippedSnowfall(isOpen: isOpen)),
+            Positioned.fill(child: child),
+          ],
+        ),
+      ),
+    );
+
+    return Container(
+      height: constants.doorHeight,
+      decoration: const BoxDecoration(
+        color: Colors.orange,
+      ),
+      child: Stack(
+        children: [
+          image,
+          snowfallAndDoors,
+        ],
+      ),
+    );
+  }
+}
+
+class ClippedSnowfall extends StatelessWidget {
+  final bool isOpen;
+  const ClippedSnowfall({super.key, required this.isOpen});
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isOpening = !isOpen;
+
+    return AnimatedOpacity(
+      opacity: isOpening ? 1 : 0,
+      duration: const Duration(milliseconds: constants.doorAnimationDuration),
+      curve: isOpening ? Curves.easeOutExpo : Curves.easeInExpo,
+      child: const ClipRect(
+        child:
+        Snowflakes(numberOfSnowflakes: 8, color: Colors.white, alpha: 120),
+      ),
+    );
+  }
+}
--- advent_calendar_app/lib/main.dart
+++ advent_calendar_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'CalendarDoor.dart';
+import 'CalendarDoorContent.dart';
 import 'constants.dart' as constants;
-import 'package:snowfall/snowfall/snowflakes.dart';
 
 void main() {
   runApp(const AdventCalendarApp());
@@ -116,71 +116,6 @@ class CalendarRow extends StatelessWidget {
           child: doorWidget,
         ),
       ],
-    );
-  }
-}
-
-class CalendarDoorContent extends StatelessWidget {
-  Widget child;
-  bool isOpen;
-  CalendarDoorContent({super.key, required this.isOpen, required this.child});
-
-  @override
-  Widget build(BuildContext context) {
-    final Widget image = SizedBox(
-      height: constants.doorHeight,
-      width: constants.doorHeight,
-      child: Image.asset(
-        'assets/default.jpeg',
-        fit: BoxFit.fill,
-      ),
-    );
-
-    final Widget snowfallAndDoors = Center(
-      // For some reason this SizedBox is needed to display the Stack.
-      child: SizedBox(
-        width: constants.doorHeight,
-        height: constants.doorHeight,
-        child: Stack(
-          children: [
-            Positioned.fill(child: ClippedSnowfall(isOpen: isOpen)),
-            Positioned.fill(child: child),
-          ],
-        ),
-      ),
-    );
-
-    return Container(
-      height: constants.doorHeight,
-      decoration: const BoxDecoration(
-        color: Colors.orange,
-      ),
-      child: Stack(
-        children: [
-          image,
-          snowfallAndDoors,
-        ],
-      ),
-    );
-  }
-}
-
-class ClippedSnowfall extends StatelessWidget {
-  final bool isOpen;
-  const ClippedSnowfall({super.key, required this.isOpen});
-
-  @override
-  Widget build(BuildContext context) {
-    final bool isOpening = !isOpen;
-
-    return AnimatedOpacity(
-      opacity: isOpening ? 1 : 0,
-      duration: const Duration(milliseconds: constants.doorAnimationDuration),
-      curve: isOpening ? Curves.easeOutExpo : Curves.easeInExpo,
-      child: const ClipRect(
-        child:
-            Snowflakes(numberOfSnowflakes: 8, color: Colors.white, alpha: 120),
-      ),
     );
   }
 }
