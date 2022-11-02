a4ec6ff32c46869eb8f45af66c4fc456d353c242
virtalas <lasvirt@gmail.com>
2022-11-02 20:52:00 +0200

    Remove DoorOpeningProgressAnimationBuilder

--- advent_calendar_app/lib/CalendarDoorContent.dart
+++ advent_calendar_app/lib/CalendarDoorContent.dart
@@ -93,7 +93,7 @@ class CalendarDoorContent extends StatelessWidget {
         child: Container(
           padding: const EdgeInsets.only(bottom: 30),
           child: AnimatedText(
-            isOpen: isOpen,
+            isDoorFullyClosed: isDoorFullyClosed,
             textInfo: textInfo!,
           ),
         ),
@@ -169,7 +169,7 @@ class CalendarDoorContent extends StatelessWidget {
             snowman,
             Positioned.fill(
               child: ClippedSnowfall(
-                isOpen: isOpen,
+                isDoorFullyClosed: isDoorFullyClosed,
                 doorNumber: doorNumber,
                 maxDoorCount: maxDoorCount,
               ),
@@ -194,34 +194,29 @@ class CalendarDoorContent extends StatelessWidget {
 }
 
 class ClippedSnowfall extends StatelessWidget {
-  final bool isOpen;
+  final bool isDoorFullyClosed;
   final int doorNumber;
   final int maxDoorCount;
   const ClippedSnowfall({
     super.key,
-    required this.isOpen,
+    required this.isDoorFullyClosed,
     required this.doorNumber,
     required this.maxDoorCount,
   });
 
   @override
   Widget build(BuildContext context) {
-    return DoorOpeningProgressAnimationBuilder(
-      isOpen: isOpen,
-      builder: (BuildContext context, double progress, Widget? child) {
-        if (progress > 0.1) {
-          return ClipRect(
-            child: Snowflakes(
-              numberOfSnowflakes: numberOfSnowflakes(doorNumber, maxDoorCount),
-              color: Colors.white,
-              alpha: 120,
-            ),
-          );
-        } else {
-          return Container();
-        }
-      },
-    );
+    if (isDoorFullyClosed) {
+      return Container();
+    } else {
+      return ClipRect(
+        child: Snowflakes(
+          numberOfSnowflakes: numberOfSnowflakes(doorNumber, maxDoorCount),
+          color: Colors.white,
+          alpha: 120,
+        ),
+      );
+    }
   }
 
   // Public for testing
@@ -250,48 +245,42 @@ class ClippedSnowfall extends StatelessWidget {
 }
 
 class AnimatedText extends StatelessWidget {
-  final bool isOpen;
+  final bool isDoorFullyClosed;
   final TextInfo textInfo;
 
   const AnimatedText({
     super.key,
-    required this.isOpen,
+    required this.isDoorFullyClosed,
     required this.textInfo,
   });
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Just use isDoorFullyClosed
-    return DoorOpeningProgressAnimationBuilder(
-      isOpen: isOpen,
-      builder: (BuildContext context, double progress, Widget? child) {
-        if (progress > 0.1) {
-          return SizedBox(
-            width: 220,
-            height: 70,
-            child: DefaultTextStyle(
-              style: textInfo.style,
-              child: IgnorePointer(
-                child: AnimatedTextKit(
-                  animatedTexts: [
-                    TypewriterAnimatedText(
-                      '${textInfo.firstRow}\n${textInfo.secondRow}',
-                      speed: Duration(milliseconds: textInfo.letterDuration),
-                      cursor: '',
-                    ),
-                  ],
-                  pause: const Duration(milliseconds: 0),
-                  displayFullTextOnTap: false,
-                  repeatForever: false,
-                  totalRepeatCount: 1,
+    if (isDoorFullyClosed) {
+      return Container();
+    } else {
+      return SizedBox(
+        width: 220,
+        height: 70,
+        child: DefaultTextStyle(
+          style: textInfo.style,
+          child: IgnorePointer(
+            child: AnimatedTextKit(
+              animatedTexts: [
+                TypewriterAnimatedText(
+                  '${textInfo.firstRow}\n${textInfo.secondRow}',
+                  speed: Duration(milliseconds: textInfo.letterDuration),
+                  cursor: '',
                 ),
-              ),
+              ],
+              pause: const Duration(milliseconds: 0),
+              displayFullTextOnTap: false,
+              repeatForever: false,
+              totalRepeatCount: 1,
             ),
-          );
-        } else {
-          return Container();
-        }
-      },
-    );
+          ),
+        ),
+      );
+    }
   }
 }
--- advent_calendar_app/lib/utils.dart
+++ advent_calendar_app/lib/utils.dart
@@ -7,26 +7,3 @@ int daysBetween(DateTime from, DateTime to) {
   to = DateTime(to.year, to.month, to.day);
   return (to.difference(from).inHours / 24).round();
 }
-
-class DoorOpeningProgressAnimationBuilder extends StatelessWidget {
-  final bool isOpen;
-  final Function builder;
-  const DoorOpeningProgressAnimationBuilder({super.key, required this.isOpen, required this.builder});
-
-  @override
-  Widget build(BuildContext context) {
-    const Duration duration = Duration(milliseconds: 170);
-
-    return TweenAnimationBuilder(
-      duration: const Duration(milliseconds: constants.doorAnimationDuration),
-      curve: constants.doorAnimationCurve,
-      tween: Tween<double>(
-        begin: isOpen ? 1 : 0,
-        end: isOpen ? 0 : 1,
-      ),
-      builder: (BuildContext context, double progress, Widget? child) {
-        return builder(context, progress, child);
-      },
-    );
-  }
-}
