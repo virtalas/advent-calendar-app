6fc8651b023a32f998c6826cb51af0bd30ceaaf6
virtalas <lasvirt@gmail.com>
2023-11-06 17:17:49 +0200

    Make it build for MacOS

--- advent_calendar_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ advent_calendar_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,12 @@ import Foundation
 
 import audio_session
 import just_audio
-import path_provider_macos
+import path_provider_foundation
+import rive_common
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
 }
